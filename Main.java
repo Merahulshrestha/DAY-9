e9576aa954fa0b9fc500c621c4029b248414468d
Rahul Shrestha <113322042+Merahulshrestha@users.noreply.github.com>
2022-11-02 15:17:28 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    
+    // declaring array
+    int num[] = new int[5];
+    // using for loop to declare data for num array
+    for(int j=0; j<num.length;j++){
+      System.out.print(num[j] = j);
+    }
+  }
+}
